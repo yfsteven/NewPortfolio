39155e1c5df205b50be5d4ad0edd7014a871399a
yfsteven <152656884+yfsteven@users.noreply.github.com~>
2025-05-11 12:21:14 -0400

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import node from '@astrojs/node';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  adapter: vercel()
+});
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    darkMode: 'class', // Enable class-based dark mode
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
